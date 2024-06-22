2d404757528977f4c2f68caf36db6b745164ccf0
asm1028 <117651049+asm1028@users.noreply.github.com>
2024-06-22 23:50:45 +0200

    [Update] Se añade carácteres máximos dentro del autocompletado.

--- lib/utils/autocomplete_products.dart
+++ lib/utils/autocomplete_products.dart
@@ -1,12 +1,14 @@
+// lib/utils/custom_autocomplete.dart
 import 'package:flutter/material.dart';
 import 'package:easy_autocomplete/easy_autocomplete.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class CustomAutocomplete extends StatelessWidget {
+class CustomAutocomplete extends StatefulWidget {
   final TextEditingController controller;
   final List<String> suggestions;
   final Function(String)? onChanged;
   final FormFieldValidator<String>? validator;
+  final int maxLength;
 
   CustomAutocomplete({
     Key? key,
@@ -14,37 +16,56 @@ class CustomAutocomplete extends StatelessWidget {
     required this.suggestions,
     this.onChanged,
     this.validator,
+    this.maxLength = 40,
   }) : super(key: key);
 
   @override
+  _CustomAutocompleteState createState() => _CustomAutocompleteState();
+}
+
+class _CustomAutocompleteState extends State<CustomAutocomplete> {
+  @override
   Widget build(BuildContext context) {
-    return EasyAutocomplete(
-      controller: controller,
-      suggestions: suggestions,
-      decoration: InputDecoration(
-        hintText: 'Nombre del producto',
-        hintStyle: TextStyle(color: Colors.grey[550]),
-        border: OutlineInputBorder(borderRadius: BorderRadius.circular(10.0)),
-        prefixIcon: Icon(FontAwesomeIcons.carrot, color: Color.fromARGB(255, 192, 70, 70)),
-      ),
-      suggestionBuilder: (data) {
-        return Container(
-          padding: EdgeInsets.all(10),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            border: Border(bottom: BorderSide(color: Colors.grey[300]!))
-          ),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: [
+        Align(
+          alignment: Alignment.centerRight,
           child: Text(
-            data,
-            style: TextStyle(
-              fontSize: 16,
-              color: Colors.black,
-            )
-          )
-        );
-      },
-      onChanged: onChanged,
-      validator: validator,
+            '${widget.maxLength - widget.controller.text.length} caracteres restantes',
+            style: TextStyle(fontSize: 14)
+          ),
+        ),
+        SizedBox(height: 5),
+        EasyAutocomplete(
+          controller: widget.controller,
+          suggestions: widget.suggestions,
+          decoration: InputDecoration(
+            hintText: 'Nombre del producto',
+            hintStyle: TextStyle(color: Colors.grey[550]),
+            border: OutlineInputBorder(borderRadius: BorderRadius.circular(10.0)),
+            prefixIcon: Icon(FontAwesomeIcons.carrot, color: Color.fromARGB(255, 192, 70, 70)),
+          ),
+          suggestionBuilder: (data) {
+            return Container(
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border(bottom: BorderSide(color: Colors.grey[300]!))
+              ),
+              child: Text(
+                data,
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.black,
+                )
+              )
+            );
+          },
+          onChanged: widget.onChanged,
+          validator: widget.validator,
+        ),
+      ],
     );
   }
 }
