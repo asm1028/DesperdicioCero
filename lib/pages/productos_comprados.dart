33c9bab019760b7824820f0295e9468172937512
asm1028 <117651049+asm1028@users.noreply.github.com>
2024-08-31 00:15:22 +0200

    [Update] Closes #31. Finalización de la mejora de productos comprados.

--- lib/pages/productos_comprados.dart
+++ lib/pages/productos_comprados.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -22,95 +23,106 @@ class ProductosComprados extends StatelessWidget {
       builder: (BuildContext context) {
         return StatefulBuilder(
           builder: (context, setState) {
-            return AlertDialog(
-              title: Text('Editar Producto'),
-              content: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  TextField(
-                    controller: nameController,
-                    decoration: InputDecoration(hintText: 'Nombre del producto'),
-                  ),
-                  SizedBox(height: 8.0),
-                  Row(
-                    children: [
-                      Text('Fecha de caducidad: '),
-                      TextButton(
-                        onPressed: () async {
-                          final DateTime? picked = await showDatePicker(
-                            context: context,
-                            initialDate: selectedDate,
-                            firstDate: DateTime.now(),
-                            lastDate: DateTime(2050),
-                          );
-                          if (picked != null && picked != selectedDate) {
-                            setState(() {
-                              selectedDate = picked;
-                              additionalDays = 0; // Reset additional days if date is picked
-                            });
-                          }
-                        },
-                        child: Text(DateFormat('dd/MM/yyyy').format(selectedDate.add(Duration(days: additionalDays)))),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 8.0),
-                  Text('Días adicionales:'),
-                  SizedBox(
-                    height: 100,
-                    child: CupertinoPicker(
-                      itemExtent: 32.0,
-                      onSelectedItemChanged: (int value) {
-                        setState(() {
-                          additionalDays = value;
-                        });
-                      },
-                      children: List<Widget>.generate(31, (int index) {
-                        return Center(
-                          child: Text(index.toString()),
-                        );
-                      }),
+            return WillPopScope(
+              onWillPop: () async {
+                Navigator.of(context).pop();
+                Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(builder: (BuildContext context) => ProductosComprados()),
+                );
+                return true;
+              },
+              child: AlertDialog(
+                title: Text('Editar Producto'),
+                content: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    TextField(
+                      controller: nameController,
+                      decoration: InputDecoration(hintText: 'Nombre del producto'),
                     ),
+                    SizedBox(height: 8.0),
+                    Row(
+                      children: [
+                        Text('Fecha de caducidad: '),
+                        TextButton(
+                          onPressed: () async {
+                            final DateTime? picked = await showDatePicker(
+                              context: context,
+                              initialDate: selectedDate,
+                              firstDate: DateTime.now(),
+                              lastDate: DateTime(2050),
+                            );
+                            if (picked != null && picked != selectedDate) {
+                              setState(() {
+                                selectedDate = picked;
+                                additionalDays = 0; // Reset additional days if date is picked
+                              });
+                            }
+                          },
+                          child: Text(DateFormat('dd/MM/yyyy').format(selectedDate.add(Duration(days: additionalDays))))),
+                      ],
+                    ),
+                    SizedBox(height: 8.0),
+                    Text('Días adicionales:'),
+                    SizedBox(
+                      height: 100,
+                      child: CupertinoPicker(
+                        itemExtent: 32.0,
+                        onSelectedItemChanged: (int value) {
+                          setState(() {
+                            additionalDays = value;
+                          });
+                        },
+                        children: List<Widget>.generate(31, (int index) {
+                          return Center(
+                            child: Text(index.toString()),
+                          );
+                        }),
+                      ),
+                    ),
+                  ],
+                ),
+                actions: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                      Navigator.of(context).pushReplacement(
+                        MaterialPageRoute(builder: (BuildContext context) => ProductosComprados()),
+                      );
+                    },
+                    child: Text('Cancelar'),
+                  ),
+                  TextButton(
+                    onPressed: () async {
+                      DateTime finalExpiryDate = selectedDate.add(Duration(days: additionalDays));
+                      String userToken = (await getUserToken())!;
+
+                      try {
+                        // Agregar producto a la base de datos "products"
+                        FirebaseFirestore.instance.collection('products').add({
+                          'name': nameController.text,
+                          'expiration': Timestamp.fromDate(finalExpiryDate),
+                          'user_token': userToken,
+                        });
+
+                        // Eliminar producto de la base de datos "purchased_products"
+                        FirebaseFirestore.instance.collection('purchased_products').doc(productId).delete();
+
+                        Navigator.of(context).pop();
+                      } catch (e) {
+                        Navigator.of(context).pop();
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            content: Text('Algo salió mal. Por favor, inténtalo de nuevo.'),
+                            duration: Duration(seconds: 2),
+                          ),
+                        );
+                      }
+                    },
+                    child: Text('Guardar'),
                   ),
                 ],
               ),
-              actions: [
-                TextButton(
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  },
-                  child: Text('Cancelar'),
-                ),
-                TextButton(
-                  onPressed: () async {
-                    DateTime finalExpiryDate = selectedDate.add(Duration(days: additionalDays));
-                    String userToken = (await getUserToken())!;
-
-                    try {
-                      // Agregar producto a la base de datos "products"
-                      FirebaseFirestore.instance.collection('products').add({
-                        'name': nameController.text,
-                        'expiration': Timestamp.fromDate(finalExpiryDate),
-                        'user_token': userToken,
-                      });
-
-                      // Eliminar producto de la base de datos "purchased_products"
-                      FirebaseFirestore.instance.collection('purchased_products').doc(productId).delete();
-
-                      Navigator.of(context).pop();
-                    } catch (e) {
-                      Navigator.of(context).pop();
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(
-                          content: Text('Algo salió mal. Por favor, inténtalo de nuevo.'),
-                          duration: Duration(seconds: 2),
-                        ),
-                      );
-                    }
-                  },
-                  child: Text('Guardar'),
-                ),
-              ],
             );
           },
         );
@@ -118,41 +130,17 @@ class ProductosComprados extends StatelessWidget {
     );
   }
 
-  void _deleteProduct(BuildContext context, String productId) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('Eliminar Producto'),
-          content: Text('¿Estás seguro de que quieres eliminar este producto?'),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: Text('Cancelar'),
-            ),
-            TextButton(
-              onPressed: () async {
-                try{
-                  FirebaseFirestore.instance.collection('purchased_products').doc(productId).delete();
-                  Navigator.of(context).pop();
-                } catch (e) {
-                  Navigator.of(context).pop();
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
-                      content: Text('Algo salió mal. Por favor, inténtalo de nuevo.'),
-                      duration: Duration(seconds: 2),
-                    ),
-                  );
-                }
-              },
-              child: Text('Eliminar'),
-            ),
-          ],
-        );
-      },
-    );
+  void _deleteProduct(BuildContext context, String productId) async {
+    try {
+      FirebaseFirestore.instance.collection('purchased_products').doc(productId).delete();
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Algo salió mal. Por favor, inténtalo de nuevo.'),
+          duration: Duration(seconds: 2),
+        ),
+      );
+    }
   }
 
   @override
@@ -220,8 +208,10 @@ class ProductosComprados extends StatelessWidget {
                     ),
                     onDismissed: (direction) {
                       if (direction == DismissDirection.startToEnd) {
+                        // Si se desliza hacia la derecha, abrir el editor
                         _editProduct(context, document.id, data['name']);
                       } else {
+                        // Si se desliza hacia la izquierda, intentar eliminar
                         _deleteProduct(context, document.id);
                       }
                     },
