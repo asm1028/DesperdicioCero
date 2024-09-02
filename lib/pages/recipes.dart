dcccf2d991b0cad5d9d618da4ee604a944c7770c
asm1028 <117651049+asm1028@users.noreply.github.com>
2024-09-03 00:17:48 +0200

    [Update] #23 Actualizada la página principal de recetas con un toque más visual.

--- lib/pages/recipes.dart
+++ lib/pages/recipes.dart
@@ -1,31 +1,34 @@
+import 'package:desperdiciocero/pages/recipes_all.dart';
+import 'package:desperdiciocero/pages/recipes_recommendations.dart';
 import 'package:flutter/material.dart';
 
-class Recipes extends StatelessWidget {
-  Recipes({super.key});
+class RecipesPage extends StatefulWidget {
+  RecipesPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context){
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Recetas',
-          style: TextStyle(
-            color: Colors.white,
+  _RecipesPageState createState() => _RecipesPageState();
+}
+
+class _RecipesPageState extends State<RecipesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2, // Número de pestañas
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Recetas'),
+          backgroundColor: const Color.fromARGB(255, 255, 212, 38),
+          bottom: TabBar(
+            tabs: [
+              Tab(text: 'Todas las Recetas'),
+              Tab(text: 'Recomendaciones'),
+            ],
           ),
         ),
-        backgroundColor: const Color.fromARGB(255, 255, 212, 38),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+        body: TabBarView(
           children: [
-            Text(
-              'Recetas',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
+            AllRecipes(), // Para la pestaña de Todas las Recetas
+            RecommendationsRecipesPage(), // Para la pestaña de Recomendaciones
           ],
         ),
       ),
