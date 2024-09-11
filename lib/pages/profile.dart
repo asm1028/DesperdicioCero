98840728d2bc95fea68d7ba90911da3e62f15dd0
asm1028 <117651049+asm1028@users.noreply.github.com>
2024-09-11 23:36:17 +0200

    [Add] Close #18 .Se añade la funcionalidad de cerrar sesión cuando hay una sesión activa.

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,8 +1,11 @@
-import 'package:desperdiciocero/pages/login.dart';
-import 'package:desperdiciocero/pages/register.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:uuid/uuid.dart';
+import 'package:desperdiciocero/pages/login.dart';
+import 'package:desperdiciocero/pages/primera_pagina.dart';
+import 'package:desperdiciocero/pages/register.dart';
 
 class Profile extends StatefulWidget {
   Profile({super.key});
@@ -12,15 +15,23 @@ class Profile extends StatefulWidget {
 }
 
 class _ProfileState extends State<Profile> {
-  String greeting = ''; // Mensaje de bienvenida
+  String greeting = '';
   bool hasName = false;
+  bool isLoggedIn = false;
 
   @override
   void initState() {
     super.initState();
+    checkLoggedInState();
     loadUserData();
   }
 
+  void checkLoggedInState() async {
+    var user = FirebaseAuth.instance.currentUser;
+    setState(() {
+      isLoggedIn = user != null;
+    });
+  }
 
   /// Carga los datos del usuario.
   ///
@@ -36,13 +47,11 @@ class _ProfileState extends State<Profile> {
     String? userToken = prefs.getString('userToken');
     if (userToken != null) {
       var userDoc = await FirebaseFirestore.instance.collection('users').doc(userToken).get();
-      if (userDoc.exists) {
-        if (userDoc.data()!.containsKey('name')) {
-          setState(() {
-            greeting = 'Hola, ${userDoc.data()!['name']}';
-            hasName = true;
-          });
-        }
+      if (userDoc.exists && userDoc.data()!.containsKey('name')) {
+        setState(() {
+          greeting = 'Hola, ${userDoc.data()!['name']}';
+          hasName = true;
+        });
       }
     }
     if (!hasName) {
@@ -50,6 +59,25 @@ class _ProfileState extends State<Profile> {
         greeting = '¿Quieres iniciar sesión para cargar los productos guardados o registrarte para guardar los productos actuales?';
       });
     }
+  }
+
+  void signOut() async {
+    await FirebaseAuth.instance.signOut();
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? backupToken = prefs.getString('backupToken');
+
+    if (backupToken != null && backupToken.isNotEmpty) {
+      await prefs.setString('userToken', backupToken);
+    } else {
+      String newUserToken = Uuid().v4();
+      await prefs.setString('userToken', newUserToken);
+      await FirebaseFirestore.instance.collection('users').doc(newUserToken).set({
+        'userToken': newUserToken,
+        'created_at': FieldValue.serverTimestamp(),
+      });
+    }
+
+    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => PrimeraPagina()));
   }
 
   @override
@@ -72,33 +100,38 @@ class _ProfileState extends State<Profile> {
                 textAlign: TextAlign.center,
               ),
             ),
-            SizedBox(height: 100),  // Añadido para crear más espacio entre el texto y los botones
+            SizedBox(height: 50),  // Añadido para crear más espacio entre el texto y los botones
             Card(
-              margin: EdgeInsets.symmetric(horizontal: 20),
+              margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
               child: Padding(
                 padding: EdgeInsets.all(16),
                 child: Column(
                   mainAxisSize: MainAxisSize.min,
                   children: [
-                    Text('Acciones de usuario', style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+                    Text('Acciones de usuario', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
                     SizedBox(height: 20),
-                    ElevatedButton.icon(
-                      icon: Icon(Icons.login, size: 24),
-                      label: Text('Iniciar sesión', style: TextStyle(fontSize: 16)),
-                      onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (context) => LoginScreen())),
-                      style: ElevatedButton.styleFrom(
-                        minimumSize: Size(double.infinity, 50)
+                    if (!isLoggedIn) ...[
+                      ElevatedButton.icon(
+                        icon: Icon(Icons.login, size: 24),
+                        label: Text('Iniciar sesión', style: TextStyle(fontSize: 16)),
+                        onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (context) => LoginScreen())),
+                        style: ElevatedButton.styleFrom(minimumSize: Size(double.infinity, 50)),
                       ),
-                    ),
-                    SizedBox(height: 20),
-                    ElevatedButton.icon(
-                      icon: Icon(Icons.app_registration, size: 24),
-                      label: Text('Registrarse', style: TextStyle(fontSize: 16)),
-                      onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (context) => RegisterScreen())),
-                      style: ElevatedButton.styleFrom(
-                        minimumSize: Size(double.infinity, 50)
+                      SizedBox(height: 20),
+                      ElevatedButton.icon(
+                        icon: Icon(Icons.app_registration, size: 24),
+                        label: Text('Registrarse', style: TextStyle(fontSize: 16)),
+                        onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (context) => RegisterScreen())),
+                        style: ElevatedButton.styleFrom(minimumSize: Size(double.infinity, 50)),
                       ),
-                    ),
+                    ] else ...[
+                      ElevatedButton.icon(
+                        icon: Icon(Icons.logout),
+                        label: Text('Cerrar sesión', style: TextStyle(fontSize: 16)),
+                        onPressed: signOut,
+                        style: ElevatedButton.styleFrom(minimumSize: Size(double.infinity, 50)),
+                      ),
+                    ],
                   ],
                 ),
               ),
